20737b434669f93fb8a3cf8734929ba9f0c3c599
RobsonTrasel <robson.trasel@gmail.com>
2023-08-17 22:50:07 -0300

    feature(config): add more methods to implement

--- config/app.js
+++ config/app.js
@@ -1,11 +1,55 @@
 export class App {
     constructor() {
+        // Environment: Can be 'dev', 'prod', 'staging', etc.
         this.env = process.env.APP_ENV || 'dev';
+
+        // Timezone & Locale
         this.timezone = process.env.APP_TIMEZONE || 'UTC';
-        this.locale = process.APP_LOCALE || 'en'
+        this.locale = process.env.APP_LOCALE || 'en';
+
+        // Optimization
         this.optimization = {
             compression: process.env.APP_COMPRESSION_ENABLED || true,
-            minification: process.env.APP_MINIFICATION_ENABLED || true
+            minification: process.env.APP_MINIFICATION_ENABLED || true,
+            lazyLoading: process.env.APP_LAZY_LOADING_ENABLED || true
+        };
+
+        // Security
+        this.security = {
+            cors: {
+                enabled: process.env.CORS_ENABLED || true,
+                origins: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['*']
+            },
+            rateLimit: {
+                enabled: process.env.RATE_LIMIT_ENABLED || true,
+                maxRequests: process.env.RATE_LIMIT_MAX || 1000,
+                window: process.env.RATE_LIMIT_WINDOW || 15 * 60 * 1000 // 15 minutes
+            }
+        };
+
+        // Session & Cookies
+        this.session = {
+            cookieName: process.env.SESSION_COOKIE_NAME || 'nexaframe_session',
+            expiration: process.env.SESSION_EXPIRATION || 24 * 60 * 60 * 1000, // 24 hours
+            secure: process.env.SESSION_SECURE || false // true for HTTPS only
+        };
+
+        // Logging
+        this.logging = {
+            level: process.env.LOGGING_LEVEL || 'info',
+            format: process.env.LOGGING_FORMAT || 'combined'
+        };
+
+        // Assets & Static Files
+        this.assets = {
+            path: process.env.ASSETS_PATH || '/public',
+            cacheControl: process.env.ASSETS_CACHE_CONTROL || 'public, max-age=31536000' // 1 year
+        };
+
+        // API Settings
+        this.api = {
+            prefix: process.env.API_PREFIX || '/api',
+            version: process.env.API_VERSION || 'v1'
         };
     }
 
@@ -79,5 +123,45 @@ export class App {
      */
     getOptimizationSettings() {
         return this.optimization;
+    }
+
+    /**
+     * Get the security settings.
+     * @returns {object} - Object with security settings.
+     */
+    getSecuritySettings() {
+        return this.security;
+    }
+
+    /**
+     * Get the session settings.
+     * @returns {object} - Object with session settings.
+     */
+    getSessionSettings() {
+        return this.session;
+    }
+
+    /**
+     * Get the logging settings.
+     * @returns {object} - Object with logging settings.
+     */
+    getLoggingSettings() {
+        return this.logging;
+    }
+
+    /**
+     * Get the assets settings.
+     * @returns {object} - Object with assets settings.
+     */
+    getAssetsSettings() {
+        return this.assets;
+    }
+
+    /**
+     * Get the API settings.
+     * @returns {object} - Object with API settings.
+     */
+    getApiSettings() {
+        return this.api;
     }
 }
